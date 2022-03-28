918911e28109995c5cdfd55c867b3ff17df1627b
David Volm <dvolm359@uwsp.edu>
2022-03-28 12:17:22 -0500

    THANKS JULIA !!!!

--- Projects/LocalDB assign/Connection.cs
+++ Projects/LocalDB assign/Connection.cs
@@ -5,7 +5,7 @@ namespace LocalDB_assign
     public static class Connection
     {
         public static string ConnectionString
-            => @"Data Source=(LocalDB)MSSQLLocalDB;
+            => @"Data Source=(LocalDB)\MSSQLLocalDB;
                 AttachDbFilename='|DataDirectory|\sampleDatabase.mdf';Integrated Security=True";
     }
 }
